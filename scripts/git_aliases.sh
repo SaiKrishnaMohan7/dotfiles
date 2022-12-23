f3fd46e1d9146d240b0fc2d47771e28b15262ae2
Sai Krishna Mohan <m.saikrishna.msk@gmail.com>
2022-12-22 23:14:49 -0500

    Add git alias fn to work with git worktrees
    - This commit adds a new alias cloneRepo that makes working with
    git worktrees easier
    While working on this I found that when I try to source the files
    for the terminal to pick changes, I get dir not found, needs investigation

--- scripts/git_aliases.sh
+++ scripts/git_aliases.sh
@@ -16,9 +16,6 @@ alias gwa="gw add"
 alias gwl="gw list"
 alias gwr="gw remove"
 
-#shows the last 10 branches you were on
-alias branchhistory="git for-each-ref --sort=committerdate refs/heads/ --format='%(refname) %(committerdate) %(authorname)' | sed 's/refs\/heads\///g' | awk '{print \$1}' | tail"
-
 # shows you the current branch
 alias gbr="git branch"
 
@@ -32,21 +29,16 @@ alias gstash="git stash" # store current branch temporarily
 # pull out currently stashed branch: merge into whatever branch is currently open
 alias gapply="git stash apply"
 
-# see differences between given branch and develop branch
-function g_diff {
-	git diff $1 develop | ack '^-{3}' | awk '{print $2}'
-}
+function cloneRepo () {
+	echo "Clone Repos to use with git worktrees; \n"
+	vared -p 'Directory Name: ' -c dirName
+	vared -p 'Git Url of repo: ' -c gitCloneUrl
 
-# Works with the above PS1 export.
-parse_git_branch() {
-	git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+	echo "Creating Directory, $dirName... \n"
+	mkdir $dirName
+	echo "Cloning into $dirName and creating a .bare directory... \n"
+	git clone --bare $gitCloneUrl $dirName/.bare
+	echo "Creating .git file in $dirName with gitdir set to ./bare \n"
+	echo "gitdir: ./.bare" > $dirName/.git
+	echo "DONE..."
 }
-# Returns the current git branch.
-currentBranch () {
-	git rev-parse --abbrev-ref HEAD
-}
-# Change terminal prompt to show the git branch.
-export PS1="\W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
-
-# List GIT branches in the order of creation
-alias gbl="git for-each-ref --sort=committerdate refs/heads/ --format='%(committerdate:short) %(authorname) %(refname:short)'"
