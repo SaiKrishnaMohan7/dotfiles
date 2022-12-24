02745c1c6f954865ab94fe98e95e55d385e0f78d
Sai Mohan <sai.mohan@aquanow.io>
2022-12-23 19:50:58 -0500

    [feat] Add new folder nav functions
    - These folder nav functions switch me into Work dir and
    set the right git creds for that folder
    - Same is that case with goPersonal that swtiches to
    personal git projects and sets the got creds

--- scripts/folder_nav_aliases.sh
+++ scripts/folder_nav_aliases.sh
@@ -3,11 +3,31 @@
 ####################################################################################
 # FOLDER NAV ALIASES
 ####################################################################################
-alias gowork="cd ~/Projects/Work"
-alias gopersonal="cd ~/Projects/Personal"
-alias goinfra="cd ~/Projects/Work/Infrastructure"
-alias goproj="cd ~/Projects"
+export PROJECTS="$HOME/Projects"
+export WORK="$PROJECTS/Work"
+export PERSONAL="$PROJECTS/Personal"
+
 # Shows all symlinks at global level
 # alias gsym="ls -al $(npm root -g)"
 # Shows all symlinks at local level
 alias lsym="ls -al ./node_modules"
+
+function goWork () {
+  echo "Swtiching to $WORK dir... \n"
+  cd $WORK
+
+  echo "Setting the right git credentials"
+  git setwork
+
+  echo "DONE"
+}
+
+function goPersonal () {
+  echo "Swtiching to $PERSONAL dir... \n"
+  cd $PERSONAL
+
+  echo "Setting the right git credentials"
+  git setpersonal
+
+  echo "DONE"
+}
