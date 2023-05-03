ec418175f48b085587db3b687743011f83a39844
Sai Mohan <sai.mohan@aquanow.io>
2023-05-03 11:22:34 -0400

    check pwd before worktree creation

--- scripts/git_aliases.sh
+++ scripts/git_aliases.sh
@@ -5,7 +5,7 @@
 ####################################################################################
 alias gaa="git add --all"
 alias gca="git commit --all -m" #"insert commit message here after a space"
-alias gnbr="git checkout -b" #"name/of-new-branch-here-after-a-space"
+alias gnbr="git checkout -b"    #"name/of-new-branch-here-after-a-space"
 alias gsa="git status"
 alias gm="git checkout main"
 alias glo="git log --oneline"
@@ -24,12 +24,12 @@ alias gpull="git pull origin" #branch-to-remote-pull-from-into-current-branch
 
 # push current branch to remote equivalent
 alias gpush="git push origin" #branch-to-push-to-after-a-space
-alias gstash="git stash" # store current branch temporarily
+alias gstash="git stash"      # store current branch temporarily
 
 # pull out currently stashed branch: merge into whatever branch is currently open
 alias gapply="git stash apply"
 
-function cloneRepo () {
+function cloneRepo() {
 	printf "Clone Repos to use with git worktrees; \n \n"
 	vared -p 'Directory Name: ' -c dirName
 
@@ -38,7 +38,7 @@ function cloneRepo () {
 	printf "Cloning into $dirName and creating a .bare directory... \n \n"
 	git clone --bare $1 $dirName/.bare
 	printf "Creating .git file in $dirName with gitdir set to ./bare \n \n"
-	printf "gitdir: ./.bare" > $dirName/.git
+	printf "gitdir: ./.bare" >$dirName/.git
 	printf "Change dir to $dirName/.bare to run git commads \n \n"
 	cd $dirName/.bare
 	# Explicitly sets the remote origin fetch so we can fetch remote branches
@@ -51,12 +51,16 @@ function cloneRepo () {
 }
 
 function newWorktree() {
+	if [[ $PWD != "/Users/saimohan/Projects/Work/aq-monorepo" ]]; then
+		echo "Dir root is not right! Exiting..."
+		return 1
+	fi
+
 	vared -p 'branch name: ' -c branchName
 	vared -p 'folderNameOrPath: ' -c folderNameOrPath
 	vared -p 'origin branch name: ' -c originBranchName
 	vared -p 'app name: ' -c appName
 
-# Check pwd
 	printf "###Creating new worktree in folder: $folderNameOrPath, branch: $branchName, branchOffOf: $originBranchName \n \n"
 	gwa -b $branchName $folderNameOrPath $originBranchName
 
@@ -94,7 +98,8 @@ function rmWorktree() {
 
 	printf "deleting folder: $folderNameOrPathToDelete and branch: $branchNameToRemove \n \n"
 
-	gwr $folderNameOrPathToDelete; git branch -D $branchNameToRemove
+	gwr $folderNameOrPathToDelete
+	git branch -D $branchNameToRemove
 
 	printf "DONE..."
 }
