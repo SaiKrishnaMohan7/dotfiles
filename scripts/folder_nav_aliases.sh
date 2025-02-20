6c4b613c465fad7da5f52aae9da8e4cee2b481f8
Sai Krishna Mohan <m.saikrishna.msk@gmail.com>
2025-02-20 14:27:59 -0500

    update folder nav aliases script
    - Doing this after encountering mac ssh client not using the right key for the right repo

--- scripts/folder_nav_aliases.sh
+++ scripts/folder_nav_aliases.sh
@@ -6,28 +6,34 @@
 export PROJECTS="$HOME/Projects"
 export WORK="$PROJECTS/Work"
 export PERSONAL="$PROJECTS/Personal"
+PERSONAL_SSH="ssh -i ~/.ssh/personal/id_ed25519 -T git@github.com"
+WORK_SSH="ssh -i ~/.ssh/work/id_ed25519 -T git@github.com"
 
 # Shows all symlinks at global level
 # alias gsym="ls -al $(npm root -g)"
 # Shows all symlinks at local level
 alias lsym="ls -al ./node_modules"
 
-function goWork () {
+function goWork() {
   echo "Swtiching to $WORK dir... \n"
   cd $WORK
 
   echo "Setting the right git credentials"
   git setwork
 
+  echo "using key: $WORK_SSH"
+
   echo "DONE"
 }
 
-function goPersonal () {
+function goPersonal() {
   echo "Swtiching to $PERSONAL dir... \n"
   cd $PERSONAL
 
   echo "Setting the right git credentials"
   git setpersonal
+
+  echo "using key: $PERSONAL_SSH"
 
   echo "DONE"
 }
